0779b11324591585ba80c2e2a5e7ee67e4ddeba5
saifulalm <ipoela@gmail.com>
2022-02-03 10:42:17 +0700

    Update Modul Production NJP

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -64,7 +64,7 @@ class UsersImport implements ToModel, WithHeadingRow
         }
 
         $request = array('id' => $id, 'pin' => $pin, 'user' => $Username, 'pass' => $Password, 'kodeproduk' => $kodeproduk, 'tujuan' => $tujuan, 'idtrx' => $idtrx, 'counter' => 1);
-        dd($request);
+
         $response = Curl::to('http://112.78.139.26:2222/api/h2h')
             ->withHeaders(array('Accept: application/json', 'Content-Type: application/json'))
             ->withdata($request)
